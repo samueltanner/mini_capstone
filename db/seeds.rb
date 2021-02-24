ab844318045dc2136379376b254dc5f7f8ee0d33
Sam Tanner <Samuel.m.s.tanner@gmail.com>
2021-02-24 11:52:40 -0800

    adds two product to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#product = Product.new(name: "tennis ball", price: "3", img_url: "https://images-na.ssl-images-amazon.com/images/I/61oT0J2ipHL._AC_UL1000_.jpg", description: "a perfect tennis ball for tennis or your dog!")
+
+product = Product.new(name: "spam", price: "3", img_url: "https://www.spam.com/wp-content/uploads/2019/09/image-product_spam-classic-12oz-420x420.png", description: "You know you want some musubi")
